bb82d52943acfab303670673441d5d05e281dfb7
Javier Novo <javier.novo@appentra.com>
2020-10-15 15:30:13 +0200

    PWD006, PWR011 & PWR013 examples

---
+++ PWD006/example-omp-2.c
@@ -0,0 +1,12 @@
+
+// PWD006: Missing deep copy of non-contiguous data to the GPU
+// https://www.appentra.com/knowledge/checks/pwd006
+
+#include <stdlib.h>
+
+void foo(int **A) {
+   #pragma omp target teams distribute parallel for map(tofrom:A[0:10][0:10])
+   for (size_t i = 0; i < 10; i++) {
+    A[i][i] += i;
+   }
+}
---
+++ PWR013/example-omp.c
@@ -0,0 +1,10 @@
+// PWR013: Avoid copying unused variables to the GPU
+// https://www.appentra.com/knowledge/checks/pwr013
+
+void example(double *A, double *B, double *C) {
+   #pragma omp target teams distribute parallel for schedule(auto) \
+   shared(A, B)  map(to: A[0:100], B[0:100]) map(tofrom: C[0:100])
+   for (int i = 0; i < 100; i++) {
+      C[i] += A[i];
+   }
+}
