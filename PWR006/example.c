91acb228c0d152bc5d87b444bb073f4bdc23e813
Jaime Gonzalez <jaime.gonzalez@appentra.com>
2021-08-19 09:26:52 +0200

    Add PWR006 example

---
+++ PWR006/example.c
@@ -0,0 +1,15 @@
+// PWR006: Avoid privatization of read-only variables
+// https://www.appentra.com/knowledge/checks/pwr006
+
+#define SIZE 5
+
+void foo() {
+  int A[SIZE] = {1, 2, 3, 4, 5};
+  int B[SIZE] = {5, 4, 3, 2, 1};
+  int sum[SIZE];
+
+  #pragma omp parallel for shared(sum) firstprivate(A, B)
+  for (int i = 0; i < SIZE; i++) {
+    sum[i] = A[i] + B[i];
+  }
+}
