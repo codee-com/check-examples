2f16e9d62edb46d8c39fc437d178410bc6d6bfa2
Robert Esclapez <robert.esclapez@appentra.com>
2021-11-25 15:26:41 +0100

    Add check examples for PWR0{28,29,30}

---
+++ PWR029/example.c
@@ -0,0 +1,10 @@
+// PWR029: Remove integer increment preventing performance optimization
+// https://www.appentra.com/knowledge/checks/pwr029
+
+void example(float *a, float *b, unsigned size) {
+  unsigned k = 0;
+  for (unsigned i = 0; i < size; i++) {
+    b[i] = a[k] + 1;
+    k = k + 1;
+  }
+}
