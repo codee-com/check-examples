d3696da8458cc6dbcd44efeb1038f30058e7cc50
Robert Esclapez García <robert.esclapez@appentra.com>
2021-11-25 18:07:04 +0100

    Add check examples for PWR0{28,29,30}

---
+++ PWR028/example.c
@@ -0,0 +1,10 @@
+// PWR028: Remove pointer increment preventing performance optimization
+// https://www.appentra.com/knowledge/checks/pwr028
+
+void example(float *a, float *b, float *c, unsigned size, unsigned inc) {
+  float *bTemp1 = b;
+  for (unsigned i = 0; i < size; i++) {
+    c[0] += (a[i] * bTemp1[0]);
+    bTemp1 -= inc;
+  }
+}
