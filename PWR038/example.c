fcd6449b9fb4b17eee645754dbc5e3dd6c2c2a2f
Daniel Otero <daniel.otero@appentra.com>
2021-12-23 11:59:09 +0100

    Add PWR038 example

---
+++ PWR038/example.c
@@ -0,0 +1,11 @@
+// PWR038: Apply loop sectioning to improve performance
+// https://www.appentra.com/knowledge/checks/pwr038
+
+void matvec(const double *restrict *restrict mat, const double *restrict vec,
+            int rows, int cols, double *restrict result) {
+  for (int i = 0; i < rows; i++) {
+    result[i] = 0;
+    for (int j = 0; j < cols; j++)
+      result[i] += mat[i][j] * vec[j];
+  }
+}
