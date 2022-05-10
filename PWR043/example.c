78a2140c46ee1e8d628dd442dea84708edbe9bac
Jaime Gonzalez <jaime.gonzalez@appentra.com>
2022-05-10 10:36:47 +0200

    Add PWR042 and PWR043 examples

---
+++ PWR043/example.c
@@ -0,0 +1,13 @@
+// PWR043: Loop nest can benefit from loop interchange, but reduction variable
+// initialization prevents loop interchange
+// https://www.appentra.com/knowledge/checks/pwr043
+
+void loopInterchange(int n, double **A, double *B) {
+  for (int i = 0; i < n; i++) {
+    double s = 0.0;
+    for (int j = 0; j < n; j++) {
+      s += A[j][i];
+    }
+    B[i] += s;
+  }
+}
