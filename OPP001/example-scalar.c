1fd150ce29d7fe1689f801c4407c859c7f4c5e51
Sergio Vazquez <sergio.vazquez@appentra.com>
2022-11-18 13:06:31 +0100

    Fix OPP00X/example-scalar.c compilation error

--- OPP001/example-scalar.c
+++ OPP001/example-scalar.c
@@ -3,11 +3,13 @@
 // Scalar reduction pattern
 // https://www.codee.com/knowledge/patterns/scalar-reduction/
 
+#include <stdio.h>
+
 double example(double *A, int n) {
   double sum = 0;
   for (int i = 0; i < n; ++i) {
     sum += A[i];
   }
-  printf(sum);
+  printf("%f", sum);
   return sum;
 }
