65a49c465fe67cdac189035444cf564a80b18cb5
Diego Alonso <diego.alonso@appentra.com>
2023-03-28 10:24:22 +0200

    Add PWR046 example

---
+++ PWR046/example.c
@@ -0,0 +1,5 @@
+// PWR046: Replace two divisions with a division and a multiplication
+// https://www.appentra.com/knowledge/checks/pwr046
+float calc_div(float a, float b, float c) {
+  return a / b / c;
+}
