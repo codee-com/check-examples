b4ed4ade12b406a09ce27f639ad1fbe5bde695a5
Robert Esclapez <robert.esclapez@appentra.com>
2023-03-29 19:05:42 +0200

    Add PWR044 example

---
+++ PWR044/example.c
@@ -0,0 +1,6 @@
+// PWR044: Avoid unnecessary floating-point data conversions involving constants
+// https://www.appentra.com/knowledge/checks/pwr044
+float calc_const(float a) {
+    return a * 2.2;
+}
+
