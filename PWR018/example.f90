88c916918b711dc5bf3d53c380cf5bebd0912527
Diego Alonso <diego.alonso@appentra.com>
2023-05-10 13:38:06 +0200

    Add PWR018 fortran example

---
+++ PWR018/example.f90
@@ -0,0 +1,22 @@
+! PWR018: Call to recursive function within a loop may inhibit vectorization
+! https://www.codee.com/knowledge/pwr018
+
+recursive function fibonacci(n) result(fibo)
+    implicit none
+    integer, intent(in) :: n
+    integer :: fibo
+    if (n <= 2) then
+        fibo = 1
+    else
+        fibo = fibonacci(n - 1) + fibonacci(n - 2)
+    end if
+end function fibonacci
+
+subroutine example(times)
+    integer :: sum, fibonacci
+    integer:: i, times
+
+    do i = 1, times
+        sum = sum + fibonacci(i)
+    end do
+end subroutine
