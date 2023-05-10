2f342d4e6b695d259a189b25ce25ba65d845e320
Diego Alonso <diego.alonso@appentra.com>
2023-05-10 14:51:35 +0200

    Add PWR022 fortran example

---
+++ PWR022/example.f90
@@ -0,0 +1,18 @@
+! PWR022: move invariant conditional out of the loop to facilitate vectorization
+! https://www.codee.com/knowledge/pwr022
+
+subroutine example()
+    implicit none
+    integer :: n
+    integer :: a(1000), i, total
+
+    n = 100
+
+    do i = 1, n
+        if (n .lt. 10) then
+            total = total + 1
+        end if
+        a(i) = total
+    end do
+
+end subroutine
