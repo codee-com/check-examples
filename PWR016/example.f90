dbd3ebc2fb330b59fdc3bf853ae36b51fc2695eb
Diego Alonso <diego.alonso@appentra.com>
2023-05-10 13:31:58 +0200

    Add PWR016 fortran example

---
+++ PWR016/example.f90
@@ -0,0 +1,25 @@
+! PWR016: Use separate arrays instead of an Array-of-Structs
+! https://www.codee.com/knowledge/pwr016
+
+program main
+
+    type point
+        integer :: x
+        integer :: y
+        integer :: z
+    end type
+
+contains
+
+    subroutine foo()
+        type(point) :: points(100)
+        integer:: i
+
+        do i = 1, 100
+            points(i)%x = 1
+            points(i)%y = 1
+        end do
+
+    end subroutine
+
+end program
