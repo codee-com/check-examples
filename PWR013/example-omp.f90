50c1c060bb2a82c02d244c8bcb766a3a115754b8
Diego Alonso <diego.alonso@appentra.com>
2023-05-10 13:13:44 +0200

    Add PWR013 fortran example

---
+++ PWR013/example-omp.f90
@@ -0,0 +1,11 @@
+! PWR013: Avoid copying unused variables to the GPU
+! https://www.codee.com/knowledge/pwr013
+
+subroutine example(a, b, c)
+    integer, intent(inout) :: a(100), b(100), c(100)
+    integer :: i
+    !$omp target teams distribute parallel do schedule(auto) shared(A, B) map(to: a(1:100), b(1:100)) map(tofrom: c(1:100))
+    do i = 1, 100
+        c(i) = c(i) + a(i)
+    end do
+end subroutine
