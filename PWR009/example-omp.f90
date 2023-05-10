cf96722cc42a8406f72e204c82377881cd5b4a7b
Diego Alonso <diego.alonso@appentra.com>
2023-05-10 11:02:01 +0200

    Add PWR009 fortran example

---
+++ PWR009/example-omp.f90
@@ -0,0 +1,18 @@
+! PWR009: Use OpenMP teams to offload work to GPU
+! https://www.codee.com/knowledge/pwr009
+
+subroutine bar()
+    real, dimension(100,200) :: y
+    integer(8) :: i, j
+
+    !$omp target map(from: y(1:100, 1:200))
+    !$omp parallel default(none) private(i, j) shared(y)
+    !$omp do private(j) schedule(static)
+    do i = 1, 100
+      do j = 1, 200
+        y(i, j) = 0
+      end do
+    end do
+    !$omp end parallel
+    !$omp end target
+end subroutine bar
