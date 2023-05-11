4cf216410f7237e9d25dd5e66efd0efced0b0ef9
Diego Alonso <diego.alonso@appentra.com>
2023-05-11 15:29:07 +0200

    Add PWR036 fortran example

---
+++ PWR036/example.f90
@@ -0,0 +1,12 @@
+! PWR036: Avoid indirect array access to improve performance
+! https://www.codee.com/knowledge/pwr036
+
+subroutine example()
+  implicit none
+  integer :: a(100), b(100), i
+
+  do i = 1, 100
+    a(b(i)) = 0
+  end do
+
+end subroutine
