e8fef3bb7b6b1d8dd5e2827d227477ad2c4177e9
Diego Alonso <diego.alonso@appentra.com>
2023-05-11 15:27:06 +0200

    Add PWR035 fortran example

---
+++ PWR035/example.f90
@@ -0,0 +1,14 @@
+! PWR035: Avoid non-consecutive array access to improve performance
+! https://www.codee.com/knowledge/pwr035
+
+subroutine example()
+    implicit none
+    integer :: a(100, 100), i, j
+
+    do i = 1, 100
+        do j = 1, 100
+            a(j, 1) = 0
+        end do
+    end do
+
+end subroutine
