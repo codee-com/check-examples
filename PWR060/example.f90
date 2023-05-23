086e8e069dfcd553d96c2314b475c933a5e57246
Ulises Costi <ulises.costi@codee.com>
2023-05-23 21:11:39 +0200

    Add PWR060 fortran example

---
+++ PWR060/example.f90
@@ -0,0 +1,16 @@
+!PWR060 : consider loop fission to separate gather memory access pattern
+!https  : // www.codee.com/knowledge/pwr060
+
+subroutine example(a, D, X, Y, index, n)
+    implicit none
+    real(kind = 8), intent(in)::a
+    real(kind = 8), dimension(n), intent(out)::D
+    real(kind = 8), dimension(n), intent(in)::X, Y
+    integer, dimension(n), intent(in)::index
+    integer, intent(in)::n
+    integer ::i
+
+    do i = 1,n
+        D(i) = a * X(index(i)) + Y(i)
+    end do
+end subroutine example
