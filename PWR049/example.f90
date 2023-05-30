8e9622a0bab13906e414380c9429a563b433d57a
Diego Alonso <diego.alonso@appentra.com>
2023-05-30 10:30:30 +0200

    Add PWR049 examples

---
+++ PWR049/example.f90
@@ -0,0 +1,18 @@
+! PWR049: Move iterator-dependent condition outside of the loop
+! https://www.codee.com/knowledge/pwr049
+
+subroutine example(n)
+    implicit none
+    integer, intent(in) :: n
+    integer :: i, j, a(n, n), b(n, n)
+
+    do i = 1, n
+        do j = 1, n
+            if (j .eq. 1) then
+                a(j, i) = 0
+            else
+                a(j, i) = a(j - 1, i) + b(j, i)
+            end if
+        end do
+    end do
+end subroutine
