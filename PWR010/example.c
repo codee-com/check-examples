b1adc5b7ec383e7037896cfdba2a0ffdaf850ec1
Ulises Costi <ulises.costi@appentra.com>
2023-08-11 17:31:07 +0200

    Update PWR010 example code to make it simpler

--- PWR010/example.c
+++ PWR010/example.c
@@ -1,11 +1,10 @@
 // PWR010: Avoid column-major array access in C/C++
 // https://www.codee.com/knowledge/pwr010
 
-void example(int **A, int *B) {
-  for (int i = 0; i < 100; ++i) {
-    double first = A[0][i];
-    for (int j = 1; j < 100; ++j) {
-      A[j][i] = first;
+void example(int **A) {
+  for (int j = 0; j < 100; ++j) {
+    for (int i = 0; i < 100; ++i) {
+      A[i][j] = 0;
     }
   }
 }
